f269c2707e8ab8a39475148f854064939d2dbe81
debashishsamantara <51512354+debashishsamantara@users.noreply.github.com>
2019-11-14 20:02:47 +0530

    Create P16.java

---
+++ P16.java
@@ -0,0 +1,23 @@
+import java.math.BigInteger;
+public class P16
+{
+	public static void main(String args[])
+	{
+		BigInteger mul= new BigInteger("1"); 
+		BigInteger a= new BigInteger("2");
+		BigInteger c= new BigInteger("1000");
+		while(c.compareTo(new BigInteger("1"))>=0)
+		{
+			mul=mul.multiply(a);
+			c=c.subtract(BigInteger.ONE);
+		}
+		System.out.println(mul);
+		String s=mul.toString();
+		int sum=0;
+		for(int i=0; i<s.length(); i++)
+		{
+			sum=sum+(s.charAt(i)-48);
+		}
+		System.out.println(sum);
+	}
+}
